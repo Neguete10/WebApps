ab737aaaed541f203618ac3d3c38db592ba33fa9
Neguete10 <henriquemlima@hotmail.com>
2022-06-12 22:40:04 -0400

    commit

--- command.js
+++ command.js
@@ -1,23 +1,13 @@
+let recruiterInfo = [];
 
+function addRecruiterInfo() {
+  let textbox1 = document.getElementById("fname");
+  let textbox2 = document.getElementById("lname");
+  let textbox3 = document.getElementById("phone");
+  let textbox4 = document.getElementById("email");
 
-      let recruiterInfo = [];
-
-      function addRecruiterInfo(){
-
-        let textbox1 = document.getElementById('fname');
-        let textbox2 = document.getElementById('lname');
-        let textbox3 = document.getElementById('phone');
-        let textbox4 = document.getElementById('email');        
-        
-        recruiterInfo.push(textbox1.value);
-        recruiterInfo.push(textbox2.value);
-        recruiterInfo.push(textbox3.value);
-        recruiterInfo.push(textbox4.value);      
-
-        
-
-      }    
-
-      
-
- 
+  recruiterInfo.push(textbox1.value);
+  recruiterInfo.push(textbox2.value);
+  recruiterInfo.push(textbox3.value);
+  recruiterInfo.push(textbox4.value);
+}
--- mysql.js
+++ mysql.js
@@ -1,27 +1,23 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 //import * as indexHtml from  './index.html'
 //let counter = 0;
 
-var con = mysql.createConnection({  
-host: "recruiters.cyoeeorjxg6q.us-east-2.rds.amazonaws.com",  
-user: "admin",  
-password: "mydatabase",  
-database: "recruiters"  
-});  
+var con = mysql.createConnection({
+  host: "recruiters.cyoeeorjxg6q.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "mydatabase",
+  database: "recruiters",
+});
 
-con.connect(function(err) {  
-if (err) throw err;  
-console.log("Connected!");  
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
 
-
-
-var sql = "INSERT INTO recruiters (recid, fname, lname, phone, email) VALUES ('2672498', 'Henrique', 'Lima', '937-919-4413', 'henriquemlima@hotmail.com')";  
-con.query(sql,function (err, result) {  
-if (err) throw err;  
-console.log("1 record inserted");  
-})
-
-
-})
-
+  var sql =
+    "INSERT INTO recruiters (recid, fname, lname, phone, email) VALUES ('2672498', 'Henrique', 'Lima', '937-919-4413', 'henriquemlima@hotmail.com')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
